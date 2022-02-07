9d5fccaf61021cd181a18fab1fd24cef141cada5
dell <dell@DESKTOP-ISL62TG>
2022-02-07 17:39:00 +0530

    ftrtf

---
+++ src/test/java/practice_Vtiger_TestCases/TC074.java
@@ -0,0 +1,30 @@
+package practice_Vtiger_TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC074 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://localhost:8888/");
+
+		driver.manage().window().maximize();
+
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+
+		driver.findElement(By.id("submitButton")).click();
+
+		Thread.sleep(2000);
+	}
+
+}
